54ac6ecc094d384729ee1ec81f6644ac8d5d78e8
Zed Spencer-Milnes <GitHub@GingerGeek.co.uk>
2013-12-26 20:39:22 +0000

    Add Live Exchane Rate?
    May or may not work. Untested may have to be ->rate at the end instead of ['rate'].
    May just not work at all.

--- php/paypal_game_cost.php
+++ php/paypal_game_cost.php
@@ -14,12 +14,12 @@ $paypal_cost->displayDetails(TRUE);
 
 $paypal_cost->setExchangeCurrency('USD');
 $paypal_cost->setExchangeRates(array(
-	'EUR' => 1.36725,
-	'GBP' => 1.63536,
-	'AUD' => 0.89107,
-	'NOK' => 0.16259,
-	'PLN' => 0.32975,
-	'BRL' => 0.42415
+	'EUR' => json_decode(file_get_contents("http://rate-exchange.appspot.com/currency?from="+$paypal_cost->ExchangeCurrency+"&to=EUR"))['rate'],
+	'GBP' => json_decode(file_get_contents("http://rate-exchange.appspot.com/currency?from="+$paypal_cost->ExchangeCurrency+"&to=GBP"))['rate'],
+	'AUD' => json_decode(file_get_contents("http://rate-exchange.appspot.com/currency?from="+$paypal_cost->ExchangeCurrency+"&to=AUD"))['rate'],
+	'NOK' => json_decode(file_get_contents("http://rate-exchange.appspot.com/currency?from="+$paypal_cost->ExchangeCurrency+"&to=NOK"))['rate'],
+	'PLN' => json_decode(file_get_contents("http://rate-exchange.appspot.com/currency?from="+$paypal_cost->ExchangeCurrency+"&to=PLN"))['rate'],
+	'BRL' => json_decode(file_get_contents("http://rate-exchange.appspot.com/currency?from="+$paypal_cost->ExchangeCurrency+"&to=BRL"))['rate']
 ));
 
 $paypal_cost->parseData();
